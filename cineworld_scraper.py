b690a1e7952335252fc1555f99eee7b202eeefd0
CoryOConnor94 <coryoconnor1994@gmail.com>
2025-02-11 20:08:04 +0000

    Separated code into functions for better readability

--- cineworld_scraper.py
+++ cineworld_scraper.py
@@ -5,13 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from datetime import datetime
 import pandas as pd
 
 
 class CineworldBot:
 
-    def __init__(self):
+    def __init__(self, url="https://www.cineworld.ie"):
         """ Initialize driver, necessary options and lists to store scraped values"""
         self.options = Options()
         self.options.add_experimental_option("detach", True)
@@ -20,49 +19,45 @@ class CineworldBot:
         # Initialize lists to store movie names and times
         self.movie_names = []
         self.movie_times = []
+        self.url = url
+
+    def accept_cookies(self):
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        # Accept cookies
+        cookies_button = self.wait.until(EC.presence_of_element_located((By.XPATH, "//button[contains(@id, 'onetrust-accept')]")))
+        cookies_button.click()
+
+    def go_to_whats_on(self):
+        # Go to What's on page
+        whats_on = self.wait.until(EC.presence_of_element_located((By.XPATH, "//a[contains(@id, 'menu-item-whatson')]")))
+        whats_on.click()
 
     def get_movies(self):
         """ Retrieves movie names and what's on today and upcoming movie dates"""
+        # Wait for movie containers to load (assuming each movie is inside a container like a div)
+        movie_containers = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.row.qb-movie"))
+            # Update this selector based on the actual container
+        )
 
-        # URL of the website you want to scrape
-        url = "https://www.cineworld.ie"
+        # Loop through each movie container
+        for container in movie_containers:
+            # Extract the movie name from the container
+            movie_name = container.find_element(By.CSS_SELECTOR, "h3.qb-movie-name").text
+            self.movie_names.append(movie_name)
+            # Extract all movie times within the same container
+            time_elements = container.find_elements(By.CSS_SELECTOR, ".btn.btn-primary.btn-lg")
+            movie_time = [time.text for time in time_elements]  # Get the text for each time
+            self.movie_times.append(movie_time)
 
-        # Open the URL
-        self.driver.get(url)
-
-        # Accept Cookies
-        cookies_button = self.driver.find_element(By.XPATH, "//button[contains(@id, 'onetrust-accept')]")
-        cookies_button.click()
-
-        # Go to What's on page
-        whats_on = self.driver.find_element(By.XPATH, "//a[contains(@id, 'menu-item-whatson')]")
-        whats_on.click()
-        # Wait for the dynamic content to load
-        try:
-            # Wait for movie containers to load (assuming each movie is inside a container like a div)
-            movie_containers = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.row.qb-movie"))
-                # Update this selector based on the actual container
-            )
-
-            # Loop through each movie container
-            for container in movie_containers:
-                # Extract the movie name from the container
-                movie_name = container.find_element(By.CSS_SELECTOR, "h3.qb-movie-name").text
-                self.movie_names.append(movie_name)
-                # Extract all movie times within the same container
-                time_elements = container.find_elements(By.CSS_SELECTOR, ".btn.btn-primary.btn-lg")
-                movie_time = [time.text for time in time_elements]  # Get the text for each time
-                self.movie_times.append(movie_time)
-
-            # Convert to Dataframe
-            film_df = pd.DataFrame({'Film': self.movie_names, 'Film Time': self.movie_times})
-            print(film_df)
-
-        finally:
-            # Close the browser
-            self.driver.quit()
+        # Convert to Dataframe
+        film_df = pd.DataFrame({'Film': self.movie_names, 'Film Time': self.movie_times})
+        print(film_df)
+        self.driver.quit()
 
 
 cineworld_bot = CineworldBot()
+cineworld_bot.accept_cookies()
+cineworld_bot.go_to_whats_on()
 cineworld_bot.get_movies()
